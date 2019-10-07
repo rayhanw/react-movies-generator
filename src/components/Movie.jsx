b0ee95e73fdca2ba9e9d3dc162bce722b6ad915b
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-10-07 13:19:18 +0800

    add link to imdb on image

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 import movieStyles from './styles/Movie.module.css';
 
 const Movie = ({ movie }) => {
+  console.log(movie);
+
   return (
     <div className={movieStyles.movie}>
       <h2>
         {movie.Title} ({movie.Year})
       </h2>
-      <img src={movie.Poster} alt={movie.Title} width="200" height="300" />
+      <a
+        href={`https://imdb.com/title/${movie.imdbID}/`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={movie.Poster} alt={movie.Title} width="200" height="300" />
+      </a>
     </div>
   );
 };
