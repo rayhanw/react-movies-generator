72ed36808e56506f14becf7a6271967acfb8bf06
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-10-07 18:28:42 +0800

    added spinner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Loader from "react-loader-spinner";
 
-import MovieList from './MovieList';
-import Form from './Form';
+import MovieList from "./MovieList";
+import Form from "./Form";
 
-import fetchMovies from '../apis';
+import fetchMovies from "../apis";
 
-import './styles/Movies.css';
+import "./styles/Movies.css";
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
+	const [movies, setMovies] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async (e, keyword) => {
-    e.preventDefault();
+	const handleSubmit = async (e, keyword) => {
+		e.preventDefault();
+		setLoading(true);
 
-    // Call OMDB API with user input
-    const movies = await fetchMovies(keyword);
-    // Set movie list to the result of API call
-    setMovies(movies);
-  };
+		// Call OMDB API with user input
+		const movies = await fetchMovies(keyword);
+		// Set movie list to the result of API call
+		setMovies(movies);
+		setLoading(false);
+	};
 
-  return (
-    <div style={{ margin: '0 1em' }}>
-      <h1 style={{ textAlign: 'center' }}>Movies</h1>
-      <Form handleSubmit={handleSubmit} />
-      <div className="movies">
-        <MovieList movies={movies} />
-      </div>
-    </div>
-  );
+	if (loading) {
+		return (
+			<div className="spinner">
+				<Loader type="Oval" color="#FE6B8B" height={300} width={300} />
+			</div>
+		);
+	}
+
+	return (
+		<div style={{ margin: "0 1em" }}>
+			<h1 style={{ textAlign: "center" }}>Movies</h1>
+			<Form handleSubmit={handleSubmit} />
+			<div className="movies">
+				<MovieList movies={movies} />
+			</div>
+		</div>
+	);
 };
 
 export default App;
