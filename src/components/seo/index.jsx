e4f5141dbd351abcbbeda8324411d1fbefaa958c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-05-24 15:28:18 +0800

    change title

--- src/components/seo/index.jsx
+++ src/components/seo/index.jsx
@@ -2,32 +2,20 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 export default () => {
-	return (
-		<Helmet>
-			<meta charSet="utf-8" />
-			<title>React Movies Generator</title>
-			<meta
-				name="description"
-				content="A simple movie searcher, and I don't know what else to write in this tag"
-			/>
-			<link
-				rel="canonical"
-				href="https://objective-wright-1c0423.netlify.com/"
-			/>
-			{/* Facebook Open Graph Data */}
-			<meta property="og:title" content="React Movies Generator" />
-			<meta
-				property="og:description"
-				content="A simple movie searcher, and I don't know what else to write in this tag"
-			/>
-			<meta
-				property="og:url"
-				content="https://objective-wright-1c0423.netlify.com/"
-			/>
-			{/* Twitter Card Data */}
-			<meta name="twitter:card" content="summary" />
-			<meta name="twitter:site" content="@domosaurusdex" />
-			<meta name="twitter:creator" content="@domosaurusdex" />
-		</Helmet>
-	);
+  return (
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Movies search</title>
+      <meta
+        name="description"
+        content="A simple movie searcher, and I don't know what else to write in this tag"
+      />
+      {/* Facebook Open Graph Data */}
+      <meta property="og:title" content="Movies search" />
+      <meta
+        property="og:description"
+        content="A simple movie searcher, and I don't know what else to write in this tag"
+      />
+    </Helmet>
+  );
 };
